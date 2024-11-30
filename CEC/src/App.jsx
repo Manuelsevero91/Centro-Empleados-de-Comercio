659302451c7034b215adac231aabc59067b90a4c
Manuel <manusevero91@gmail.com>
2024-11-30 14:49:08 -0300

    Rutas incorporadas

--- CEC/src/App.jsx
+++ CEC/src/App.jsx
@@ -2,9 +2,17 @@ import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Home from "./Paginas/Home";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-// import Nosotros from './Paginas/Nosotros'
-// import Servicios from './Paginas/Servicios'
-// import Afiliados from './Paginas/Afiliados'
+import Licencias from './Paginas/Licencias';
+import Adicionales from './Paginas/Adicionales';
+import Categorias from './Paginas/Categorias';
+import Jubilacion from './Paginas/Jubilacion';
+import Salud from './Paginas/Salud';
+import Turismo from './Paginas/Turismo';
+import Extras from './Paginas/Extras';
+
+
+
+
 
 
 function App() {
@@ -15,9 +23,13 @@ function App() {
     
     <Routes>
       <Route path="/" element={<Home />} />
-      {/* <Route path ="/Nosotros" element={<Nosotros />} />
-      <Route path ="/Información Afiliados" element={<Afiliados />} />
-      <Route path ="/Servicios" element={<Servicios />} /> */}
+      <Route path ="/Licencias" element={<Licencias />} />
+      <Route path ="/Adicionales" element={<Adicionales />} />
+      <Route path ="/Categorias" element={<Categorias />} />
+      <Route path ="/Jubilacion" element={<Jubilacion />} />
+      <Route path ="/Salud" element={<Salud />} />
+      <Route path ="/Turismo" element={<Turismo />} />
+      <Route path ="/Extras" element={<Extras />} />
       </Routes>
 
     </>
--- CEC/src/Componentes/NavBar.jsx
+++ CEC/src/Componentes/NavBar.jsx
@@ -1,3 +1,67 @@
+// import React from "react";
+// import {
+//   Navbar,
+//   Nav,
+//   Form,
+//   FormControl,
+//   Button,
+//   Container,
+//   NavDropdown,
+// } from "react-bootstrap";
+// import "../Estilos/NavBar.css";
+// const CustomNavbar = () => {
+//   return (
+//     <Navbar bg="dark" variant="dark" expand="lg">
+//       <Container>
+//         {/* Título o Logo */}
+//         <Navbar.Brand href="#">Centro de Empleados de Comercio</Navbar.Brand>
+
+//         {/* Botón de toggle para colapsar la Navbar en pantallas pequeñas */}
+//         <Navbar.Toggle aria-controls="basic-navbar-nav" />
+
+//         {/* Links y formulario de búsqueda */}
+//         <Navbar.Collapse id="basic-navbar-nav">
+//           <Nav className="me-auto">
+//             <Nav.Link href="#home">Inicio</Nav.Link>
+
+//             {/* Dropdown para Información */}
+//             <NavDropdown title="Información" id="dropdown-informacion">
+//               <NavDropdown.Item href="#licencias">Licencias</NavDropdown.Item>
+//               <NavDropdown.Item href="#adicionales">
+//                 Adicionales
+//               </NavDropdown.Item>
+//               <NavDropdown.Item href="#categorías">Categorías</NavDropdown.Item>
+//               <NavDropdown.Item href="#jubilación">Jubilación</NavDropdown.Item>
+//             </NavDropdown>
+
+//             {/* Dropdown para Servicios */}
+//             <NavDropdown title="Servicios" id="dropdown-servicios">
+//               <NavDropdown.Item href="#salud">Salud</NavDropdown.Item>
+//               <NavDropdown.Item href="#turismo">Turismo</NavDropdown.Item>
+//               <NavDropdown.Item href="#extras">Extras</NavDropdown.Item>
+//             </NavDropdown>
+
+//             <Nav.Link href="#contact">Contacto</Nav.Link>
+//           </Nav>
+
+//           {/* Buscador */}
+//           <Form className="d-flex">
+//             <FormControl
+//               type="search"
+//               placeholder="Buscar"
+//               className="me-2"
+//               aria-label="Search"
+//             />
+//             <Button variant="outline-light">Buscar</Button>
+//           </Form>
+//         </Navbar.Collapse>
+//       </Container>
+//     </Navbar>
+//   );
+// };
+
+// export default CustomNavbar;
+
 import React from "react";
 import {
   Navbar,
@@ -8,13 +72,15 @@ import {
   Container,
   NavDropdown,
 } from "react-bootstrap";
+import { NavLink } from "react-router-dom";
 import "../Estilos/NavBar.css";
+
 const CustomNavbar = () => {
   return (
     <Navbar bg="dark" variant="dark" expand="lg">
       <Container>
         {/* Título o Logo */}
-        <Navbar.Brand href="#">Centro de Empleados de Comercio</Navbar.Brand>
+        <Navbar.Brand as={NavLink} to="/">Centro de Empleados de Comercio</Navbar.Brand>
 
         {/* Botón de toggle para colapsar la Navbar en pantallas pequeñas */}
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
@@ -22,26 +88,24 @@ const CustomNavbar = () => {
         {/* Links y formulario de búsqueda */}
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#home">Inicio</Nav.Link>
+            <Nav.Link as={NavLink} to="/">Inicio</Nav.Link>
 
             {/* Dropdown para Información */}
             <NavDropdown title="Información" id="dropdown-informacion">
-              <NavDropdown.Item href="#licencias">Licencias</NavDropdown.Item>
-              <NavDropdown.Item href="#adicionales">
-                Adicionales
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#categorías">Categorías</NavDropdown.Item>
-              <NavDropdown.Item href="#jubilación">Jubilación</NavDropdown.Item>
+              <NavDropdown.Item as={NavLink} to="/licencias">Licencias</NavDropdown.Item>
+              <NavDropdown.Item as={NavLink} to="/adicionales">Adicionales</NavDropdown.Item>
+              <NavDropdown.Item as={NavLink} to="/categorias">Categorías</NavDropdown.Item>
+              <NavDropdown.Item as={NavLink} to="/jubilacion">Jubilación</NavDropdown.Item>
             </NavDropdown>
 
             {/* Dropdown para Servicios */}
             <NavDropdown title="Servicios" id="dropdown-servicios">
-              <NavDropdown.Item href="#salud">Salud</NavDropdown.Item>
-              <NavDropdown.Item href="#turismo">Turismo</NavDropdown.Item>
-              <NavDropdown.Item href="#extras">Extras</NavDropdown.Item>
+              <NavDropdown.Item as={NavLink} to="/salud">Salud</NavDropdown.Item>
+              <NavDropdown.Item as={NavLink} to="/turismo">Turismo</NavDropdown.Item>
+              <NavDropdown.Item as={NavLink} to="/extras">Extras</NavDropdown.Item>
             </NavDropdown>
 
-            <Nav.Link href="#contact">Contacto</Nav.Link>
+            <Nav.Link as={NavLink} to="/contacto">Contacto</Nav.Link>
           </Nav>
 
           {/* Buscador */}
@@ -61,3 +125,4 @@ const CustomNavbar = () => {
 };
 
 export default CustomNavbar;
+
