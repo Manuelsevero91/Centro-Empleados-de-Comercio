363580e77620c981c7e10fdcdb913c06425b548b
Karina Desposito <kvdesposito@gmail.com>
2024-11-30 14:08:23 -0300

    inserción de drops down

--- CEC/src/Componentes/NavBar.jsx
+++ CEC/src/Componentes/NavBar.jsx
@@ -1,5 +1,13 @@
-import React from 'react';
-import { Navbar, Nav, Form, FormControl, Button, Container } from 'react-bootstrap';
+import React from "react";
+import {
+  Navbar,
+  Nav,
+  Form,
+  FormControl,
+  Button,
+  Container,
+  NavDropdown,
+} from "react-bootstrap";
 import "../Estilos/NavBar.css";
 const CustomNavbar = () => {
   return (
@@ -7,20 +15,35 @@ const CustomNavbar = () => {
       <Container>
         {/* Título o Logo */}
         <Navbar.Brand href="#">Centro de Empleados de Comercio</Navbar.Brand>
-        
+
         {/* Botón de toggle para colapsar la Navbar en pantallas pequeñas */}
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        
+
         {/* Links y formulario de búsqueda */}
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#home">Inicio</Nav.Link>          
-            <Nav.Link href="#información">Información</Nav.Link>
-            <Nav.Link href="#services">Servicios</Nav.Link>
-            <Nav.Link href="#events">Noticias</Nav.Link>
+            <Nav.Link href="#home">Inicio</Nav.Link>
+
+            {/* Dropdown para Información */}
+            <NavDropdown title="Información" id="dropdown-informacion">
+              <NavDropdown.Item href="#licencias">Licencias</NavDropdown.Item>
+              <NavDropdown.Item href="#adicionales">
+                Adicionales
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#categorías">Categorías</NavDropdown.Item>
+              <NavDropdown.Item href="#jubilación">Jubilación</NavDropdown.Item>
+            </NavDropdown>
+
+            {/* Dropdown para Servicios */}
+            <NavDropdown title="Servicios" id="dropdown-servicios">
+              <NavDropdown.Item href="#salud">Salud</NavDropdown.Item>
+              <NavDropdown.Item href="#turismo">Turismo</NavDropdown.Item>
+              <NavDropdown.Item href="#extras">Extras</NavDropdown.Item>
+            </NavDropdown>
+
             <Nav.Link href="#contact">Contacto</Nav.Link>
           </Nav>
-          
+
           {/* Buscador */}
           <Form className="d-flex">
             <FormControl
